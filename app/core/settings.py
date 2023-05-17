0d3ed2bf2d81792497516c3a0e4a5901e0200333
Hyoungsooo <aaa57403@gmail.com>
2023-05-17 22:11:57 +0900

    change settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
 
-
     'account',
     'api',
     'web',
@@ -56,9 +55,11 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'channels',
     "corsheaders",
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
